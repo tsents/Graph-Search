2bb95a0f1677aad642a687f41bb68a0fd57dc5f9
Sagie Tsentsiper <tsents.sagie@gmail.com>
2024-06-04 13:14:29 +0300

    with main, wrapper and concurrency added

--- linegraph.go
+++ linegraph.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand/v2"
+	"sync"
 )
 
 type void struct{}
@@ -33,28 +34,33 @@ type att struct {
 // idea - store Graph as map of vertex -> neighboorhood -> void (neighborhood is a set)
 func main() {
 	fmt.Println("hello world")
-	// currentTime := time.Now()
-	// folderName := currentTime.Format("2006-01-02 15:04:05")
-	// folderName = "dat/" + folderName
-	// err := os.Mkdir(folderName, 0755)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// for t := 0; t < 50; t++ {
-	// 	fmt.Println("starting search")
+	for t := 0; t < 50; t++{
+		G := Gnp(1e4, 1e-3)
+		S := Gnp(1e2, 1e-1)
+		for j := uint32(0); j < uint32(len(S)) - 1; j++{
+			AddEdge(S,j,j+1)
+		}
+		FindAllSubgraph(G,S)
+		fmt.Println("done",t)
+	}
+}
 
-	// 	file, err := os.Create(folderName + "/output" + fmt.Sprintf("%d", t) + ".txt")
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		return
-	// 	}
-	// 	defer file.Close()
-	// }
+func FindAllSubgraph(Graph graph, Subgraph graph) {
+	var wg sync.WaitGroup
+	for u := uint32(0); u < uint32(len(Graph)); u++ {
+		if Graph[u].attribute.color == Subgraph[0].attribute.color {
+			wg.Add(1)
+			go func(u uint32) {
+				RecursionSearch(Graph, Subgraph, u, 0, make([]*list, len(Subgraph)), make(map[uint32]uint32))
+				wg.Done()
+			} (u)
+		}
+	}
+	wg.Wait()
 }
 
 func RecursionSearch(Graph graph, Subgraph graph, v_g uint32, v_s uint32,
-	restrictions []*list, path map[uint32]uint32) int{
+	restrictions []*list, path map[uint32]uint32) int {
 	if _, ok := path[v_g]; ok {
 		return 0
 	}
@@ -74,16 +80,16 @@ func RecursionSearch(Graph graph, Subgraph graph, v_g uint32, v_s uint32,
 		for u_instance := restrictions[v_s+1].start; u_instance != nil; u_instance = u_instance.next {
 			targets = append(targets, u_instance.value)
 		}
-		for i := 0; i < len(targets); i++{
+		for i := 0; i < len(targets); i++ {
 			ret += RecursionSearch(Graph, Subgraph, targets[i], v_s+1, restrictions, path)
 		}
 	}
-	for u := 0; u < len(inverse_restrictions); u++{
+	for u := 0; u < len(inverse_restrictions); u++ {
 		if inverse_restrictions[u] != nil {
-			if inverse_restrictions[u].start != nil && inverse_restrictions[u].start.value == ^uint32(0){
+			if inverse_restrictions[u].start != nil && inverse_restrictions[u].start.value == ^uint32(0) {
 				restrictions[u] = nil
 			} else {
-				restrictions[u] = JoinLists(restrictions[u],inverse_restrictions[u])
+				restrictions[u] = JoinLists(restrictions[u], inverse_restrictions[u])
 			}
 		}
 	}
@@ -97,14 +103,14 @@ func UpdateRestrictions(G graph, S graph, v_g uint32, v_s uint32, restrictions [
 	for u := range S[v_s].neighborhood {
 		if restrictions[u] == nil {
 			restrictions[u] = ColoredNeighborhood(G, v_g, S[u].attribute.color)
-			el := element{^uint32(0),nil}
-			inverse_restrictions[u] = &list{&el,&el}
+			el := element{^uint32(0), nil}
+			inverse_restrictions[u] = &list{&el, &el}
 		} else {
 			var dis discriminator = func(u_instance uint32) bool {
-				_, ok := G[v_g].neighborhood[u_instance];
+				_, ok := G[v_g].neighborhood[u_instance]
 				return ok
 			}
-			restrictions[u], inverse_restrictions[u] = SplitList(restrictions[u],dis)
+			restrictions[u], inverse_restrictions[u] = SplitList(restrictions[u], dis)
 			if restrictions[u].start == nil {
 				empty = true
 			}
@@ -117,39 +123,39 @@ func ColoredNeighborhood(Graph map[uint32]vertex, u uint32, c uint8) *list {
 	output := list{nil, nil}
 	for v := range Graph[u].neighborhood {
 		if Graph[v].attribute.color == c {
-			el := element{v,nil}
-			ListAppend(&output,&el)
+			el := element{v, nil}
+			ListAppend(&output, &el)
 		}
 	}
 	return &output
 }
 
-func SplitList(l *list,which discriminator) (*list,*list){
-	l1 := &list{nil,nil}
-	l2 := &list{nil,nil}
+func SplitList(l *list, which discriminator) (*list, *list) {
+	l1 := &list{nil, nil}
+	l2 := &list{nil, nil}
 	var next *element
-	for el := l.start; el != nil; el = next{
+	for el := l.start; el != nil; el = next {
 		next = el.next
-		if which(el.value){
-			ListAppend(l1,el)
+		if which(el.value) {
+			ListAppend(l1, el)
 		} else {
-			ListAppend(l2,el)
+			ListAppend(l2, el)
 		}
 	}
-	return l1,l2
+	return l1, l2
 }
 
-func JoinLists(l1 *list,l2 *list) *list {
+func JoinLists(l1 *list, l2 *list) *list {
 	if l2 == nil {
 		return l1
 	}
 	if l1 == nil {
 		return l2
 	}
-	if l1.start == nil{
+	if l1.start == nil {
 		return l2
 	}
-	if l2.start == nil{
+	if l2.start == nil {
 		return l1
 	}
 	// fmt.Println(l1,l2)
