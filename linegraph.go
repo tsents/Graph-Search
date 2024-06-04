bd0d52dfad6ce40209157421c778e99afa5e2a16
Sagie Tsentsiper <tsents.sagie@gmail.com>
2024-06-04 09:10:54 +0300

    still doesnt fix

--- linegraph.go
+++ linegraph.go
@@ -13,17 +13,7 @@ type void struct{}
 
 type graph map[uint32]vertex
 
-type fisset []*list
-
-type element struct {
-	value uint32
-	next  *element
-}
-
-type list struct {
-	start *element
-	end   *element
-}
+type fisset [][]uint32
 
 type vertex struct {
 	attribute    att
@@ -125,55 +115,25 @@ func recursion_search(Graph graph, Subgraph graph, v_g uint32, v_s uint32,
 	}
 	path[v_g] = v_s
 
-	deep_cpy := ListsArrDeepCopy(restrictions)
-
-	func() {
-		debug_rest := ListsArrDeepCopy(restrictions)
-		inverse_restrictions1, _ := update_restrictions(Graph, Subgraph, v_g, v_s, debug_rest)
-		cpy_test := ListsArrDeepCopy(inverse_restrictions1)
-		reverse_restrictions(debug_rest, inverse_restrictions1)
-		if !AreListsArrEqual(debug_rest, restrictions) {
-			fmt.Println("bingo!")
-		}
-		inverse_restrictions2, _ := update_restrictions(Graph, Subgraph, v_g, v_s, debug_rest)
-		if !AreListsArrEqual(inverse_restrictions2, cpy_test) {
-			fmt.Println("bingo2!")
-		}
-		reverse_restrictions(debug_rest, inverse_restrictions2)
-		if !AreListsArrEqual(debug_rest, restrictions) {
-			fmt.Println("bingo3!")
-		}
-	}()
-
 	inverse_restrictions, empty := update_restrictions(Graph, Subgraph, v_g, v_s, restrictions)
 	defer reverse_restrictions(restrictions, inverse_restrictions)
-	defer func() {
-		if !AreListsArrEqual(deep_cpy, restrictions) {
-			fmt.Println("err2")
-		}
-	}()
 
 	if !empty {
-		for u_instance := restrictions[v_s+1].start; u_instance != nil; u_instance = u_instance.next {
-			recursion_search(Graph, Subgraph, u_instance.value, v_s+1, restrictions, path, file) // for some resone modifies the retrictions?! but doesnt reverse currectly
-			if !AreListsArrEqual(deep_cpy, restrictions) {
-				fmt.Println("err")
-			}
+		for i := 0; i < len(restrictions[v_s+1]); i++ {
+			recursion_search(Graph, Subgraph, restrictions[v_s+1][i], v_s+1, restrictions, path, file) // for some resone modifies the retrictions?! but doesnt reverse currectly
 		}
 	}
 	delete(path, v_g)
 }
 
 func reverse_restrictions(restrictions fisset, inverse_restrictions fisset) {
-	for u, rest_u := range inverse_restrictions { //tested and works
-		if rest_u != nil {
-			if rest_u.start != nil && rest_u.start.value == ^uint32(0) {
-				restrictions[u] = nil
-			} else {
-				join_lists(restrictions[u], rest_u)
-			}
+	for i := 0; i < len(inverse_restrictions); i++{
+		if len(inverse_restrictions[i]) > 0 && inverse_restrictions[i][0] == ^uint32(0){
+			restrictions[i] = nil
+		} else {
+			restrictions[i] = append(restrictions[i], inverse_restrictions[i]...)
 		}
-	}
+	} 
 }
 
 func update_restrictions(G graph, S graph, v_g uint32, v_s uint32, restrictions fisset) (fisset, bool) {
@@ -182,11 +142,10 @@ func update_restrictions(G graph, S graph, v_g uint32, v_s uint32, restrictions 
 	for u := range S[v_s].neighborhood {
 		if restrictions[u] == nil {
 			restrictions[u] = neighboorhood_with_color(G, v_g, S[u].attribute.color)
-			el := element{^uint32(0), nil}
-			inverse_restrictions[u] = &list{&el, &el}
+			inverse_restrictions[u] = []uint32{^uint32(0)}
 		} else {
 			restrictions[u], inverse_restrictions[u] = uninitialized_update(G, restrictions, u, v_g)
-			if restrictions[u].start == nil {
+			if len(restrictions[u]) == 0 {
 				empty = true
 			}
 		}
@@ -194,86 +153,27 @@ func update_restrictions(G graph, S graph, v_g uint32, v_s uint32, restrictions 
 	return inverse_restrictions, empty
 }
 
-func uninitialized_update(G graph, restrictions fisset, u uint32, v_g uint32) (*list, *list) {
-	temp_retriction := list{nil, nil}
-	temp_inverse := list{nil, nil}
-	var next *element
-	for u_instance := restrictions[u].start; u_instance != nil; u_instance = next {
-		next = u_instance.next
-		if _, ok := G[v_g].neighborhood[u_instance.value]; ok {
-			list_append(&temp_retriction, u_instance)
+func uninitialized_update(G graph, restrictions fisset, u uint32, v_g uint32) ([]uint32,[]uint32) {
+	temp_retriction := []uint32{}
+	temp_inverse := []uint32{}
+	for i := 0; i < len(restrictions[u]); i++{
+		if _, ok := G[v_g].neighborhood[restrictions[u][i]]; ok {
+			temp_retriction = append(temp_retriction, restrictions[u][i])
 		} else {
-			list_append(&temp_inverse, u_instance)
+			temp_inverse = append(temp_inverse, restrictions[u][i])
 		}
 	}
-	if temp_retriction.end != nil {
-		temp_retriction.end.next = nil
-	}
-	if temp_inverse.end != nil {
-		temp_inverse.end.next = nil
-	}
-
-	return &temp_retriction, &temp_inverse
+	return temp_retriction, temp_inverse
 }
 
-// modefies l1, l2 remain itself
-func join_lists(l1 *list, l2 *list) {
-	defer func() {
-		l2 = &list{nil, nil}
-		l2 = &list{nil, nil}
-	}()
-	if l2.end == nil && l2.start != nil {
-		fmt.Println("what")
-	}
-	if l1.end == nil {
-		l1.start = l2.start
-		l1.end = l2.end
-		return
-	}
-	l1.end.next = l2.start
-	l1.end = l2.end
-}
-
-func PrintList(l *list) {
-	if l == nil {
-		fmt.Print("_")
-		return
-	}
-	fmt.Print("[")
-	for el := l.start; el != nil; el = el.next {
-		fmt.Print(el.value, ", ")
-	}
-	fmt.Print("]")
-}
-
-func PrintListArr(arr []*list) {
-	fmt.Print("[")
-	for i := 0; i < len(arr); i++ {
-		PrintList(arr[i])
-	}
-	fmt.Print("]\n")
-}
-
-func list_append(l *list, el *element) {
-	el.next = nil
-	if l.start == nil {
-		l.start = el
-		l.end = el
-		return
-	}
-	l.end.next = el
-	l.end = el
-}
-
-func neighboorhood_with_color(Graph graph, u uint32, c uint8) *list {
-	output := list{nil, nil}
+func neighboorhood_with_color(Graph graph, u uint32, c uint8) []uint32 {
+	output := []uint32{}
 	for v := range Graph[u].neighborhood {
 		if Graph[v].attribute.color == c {
-			el := element{v,nil}
-			list_append(&output, &el)
+			output = append(output, v)
 		}
 	}
-	return &output
+	return output
 }
 
 // func print_graph(Graph graph) {
@@ -329,74 +229,6 @@ func VerifyResult(G graph, S graph, path map[uint32]uint32) {
 			}
 		}
 	}
-}
-
-func AreListsArrEqual(arr1 []*list, arr2 []*list) bool {
-	if len(arr1) != len(arr2) {
-		return false
-	}
-	for i := 0; i < len(arr1); i++ {
-		if !AreListsEqual(arr1[i], arr2[i]) {
-			return false
-		}
-	}
-	return true
-}
-
-func AreListsEqual(list1 *list, list2 *list) bool {
-	if list1 == nil {
-		return list2 == nil
-	}
-	if list1.start == nil {
-		return list2.start == nil
-	}
-	map1 := make(map[uint32]bool)
-	map2 := make(map[uint32]bool)
-	for e := list1.start; e != nil; e = e.next {
-		map1[e.value] = true
-	}
-
-	for e := list2.start; e != nil; e = e.next {
-		map2[e.value] = true
-	}
-
-	if len(map1) != len(map2) {
-		return false
-	}
-
-	for k := range map1 {
-		if !map2[k] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func ListsDeepCopy(l *list) *list {
-	if l == nil {
-		return nil
-	}
-	if l.start == nil {
-		return &list{nil, nil}
-	}
-	out := list{nil, nil}
-	for el := l.start; el != nil; el = el.next {
-		cpy := element{el.value,nil}
-		list_append(&out, &cpy)
-	}
-	return &out
-}
-
-func ListsArrDeepCopy(arr []*list) []*list {
-	if arr == nil {
-		return nil
-	}
-	new := make([]*list, len(arr))
-	for i := 0; i < len(arr); i++ {
-		new[i] = ListsDeepCopy(arr[i])
-	}
-	return new
 }
 
 func ReverseMap(m map[uint32]uint32) map[uint32]uint32 {
