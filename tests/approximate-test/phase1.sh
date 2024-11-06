9ece0e898c7d862f72824e5db64ec1541b5012ff
tsents <tsents.sagie@gmail.com>
2024-11-06 11:52:49 +0200

    remove old tests

--- tests/approximate-test/phase1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ((i=1; i<=16000; i+=5))
-do
-  ./subgraph_isomorphism -subset=500 -fmt=folder -start=$i -parse="%d %d" -prior=0 -subout -err=0 -out=dat/subgraphs/sub$i.output tests/fe-sphere/ > log.txt
-  # ./subgraph_isomorphism -subset=500 -fmt=folder -start=$i -parse="%d %d" -prior=0 -subout -err=5 -out=dat/subgraphs/sub$i.err tests/fe-sphere/ > log.txt
-done
